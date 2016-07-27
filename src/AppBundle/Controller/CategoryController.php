b2a9a961dbcbd8dac0746b337f4a37ff75e2750f
Xi Wang <g@g.net>
2016-07-27 17:20:26 +1200

    Category CRUD v0.1

---
+++ src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Category;
+
+/**
+ * Category controller.
+ *
+ * @Route("/category")
+ */
+class CategoryController extends Controller
+{
+    /**
+     * Lists all Category entities.
+     *
+     * @Route("/", name="category_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('AppBundle:Category')->findAll();
+
+        return $this->render('category/list.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+
+    /**
+     * Finds and displays a Category entity.
+     *
+     * @Route("/{id}", name="category_show")
+     * @Method("GET")
+     */
+    public function showAction(Category $category)
+    {
+
+        return $this->render('category/show.html.twig', array(
+            'category' => $category,
+        ));
+    }
+}
