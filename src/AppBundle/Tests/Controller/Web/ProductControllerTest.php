7e3a98b6bd4550c007fbece77cacf1d61b8c90bc
Xi Wang <g@g.net>
2016-08-03 12:09:18 +1200

    TESTS #functional: testProductCRUD, remove stub

--- src/AppBundle/Tests/Controller/Web/ProductControllerTest.php
+++ src/AppBundle/Tests/Controller/Web/ProductControllerTest.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Tests\Controller\Web;
 
 use AppBundle\Tests\BaseWebTestCase;
-use Symfony\Component\HttpKernel\Kernel;
 
 class ProductControllerTest extends BaseWebTestCase
 {
@@ -14,56 +13,41 @@ class ProductControllerTest extends BaseWebTestCase
         $this->loadFixtureFromFile();
     }
 
-    public function testListProductAction()
+    public function testProductCRUD()
     {
         $url = parent::$baseUrl . 'product';
 
         $crawler = $this->client->request('GET', $url);
         $response = $this->client->getInternalResponse();
         $this->assertEquals(200, $response->getStatus(), "Unexpected HTTP status code for GET product/");
-    }
+        $this->assertContains('Product list', $crawler->filter('body > h1')->text());
+        $this->assertGreaterThan(0, $crawler->filter('td')->count(), 'Missing element td numbers');
 
-//    public function testCompleteScenario()
-//    {
-//        // Create a new client to browse the application
-//        $client = static::createClient();
-//
-//        // Create a new entry in the database
-//        $crawler = $client->request('GET', '/product/');
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /product/");
-//        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-//
-//        // Fill in the form and submit it
-//        $form = $crawler->selectButton('Create')->form(array(
-//            'appbundle_product[field_name]'  => 'Test',
-//            // ... other fields to fill
-//        ));
-//
-//        $client->submit($form);
-//        $crawler = $client->followRedirect();
-//
-//        // Check data in the show view
-//        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-//
-//        // Edit the entity
-//        $crawler = $client->click($crawler->selectLink('Edit')->link());
-//
-//        $form = $crawler->selectButton('Update')->form(array(
-//            'appbundle_product[field_name]'  => 'Foo',
-//            // ... other fields to fill
-//        ));
-//
-//        $client->submit($form);
-//        $crawler = $client->followRedirect();
-//
-//        // Check the element contains an attribute with value equals "Foo"
-//        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-//
-//        // Delete the entity
-//        $client->submit($crawler->selectButton('Delete')->form());
-//        $crawler = $client->followRedirect();
-//
-//        // Check the entity has been delete on the list
-//        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
-//    }
+        $crawler = $this->client->click($crawler->selectLink('Create a new entry')->link());
+
+        // Fill in the form and submit it
+        $form = $crawler->selectButton('Create')->form(array(
+            'product[name]'  => 'acme_product2',
+            'product[price]'  => 100,
+            'product[description]'  => 'acme_product_description',
+            'product[image]'  => 'acme_product_image',
+            'product[stock]'  => 10,
+        ));
+        $crawler = $this->client->submit($form);
+
+        // Show
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("acme_product2")')->count(), 'Missing element td:contains("aceme_product2")');
+
+        // Edit
+        $crawler = $this->client->click($crawler->selectLink('Edit')->link());
+        $form = $crawler->selectButton('Edit')->form(array(
+            'product[name]'  => 'acme_product3',
+        ));
+        $crawler = $this->client->submit($form);
+        $this->assertGreaterThan(0, $crawler->filter('[value="acme_product3"]')->count(), 'Missing element [value="acme_product3"]');
+
+        // Delete
+        $crawler = $this->client->submit($crawler->selectButton('Delete')->form());
+        $this->assertNotRegExp('/acme_product3/', $this->client->getResponse()->getContent());
+    }
 }
