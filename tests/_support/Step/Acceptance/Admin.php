3e28e14a10a124213a5a67c6704065119617a603
Xi Wang <g@g.net>
2016-10-20 12:34:55 +1300

    DEBUG #minor: remove SessionSnapshot in cc step object (to allow proper setup and teardown in AdminCest)

--- tests/_support/Step/Acceptance/Admin.php
+++ tests/_support/Step/Acceptance/Admin.php
@@ -6,18 +6,12 @@ class Admin extends \AcceptanceTester
     public function loginAsAdmin()
     {
         $I = $this;
-        // if snapshot exists - skipping login
-        if ($I->loadSessionSnapshot('login')) {
-            return;
-        }
         // logging in
         $I->amOnPage('/admin/login');
         $I->fillField('#username', 'admin');
         $I->fillField('#password', 'admin');
         $I->click('Log in');
         $I->waitForText('Manages all web content and projects related to the website');
-        // saving snapshot
-        $I->saveSessionSnapshot('login');
     }
 
     public function logoutAsAdmin()
