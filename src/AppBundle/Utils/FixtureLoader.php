5aa0f0cdf6f099a3b469f0e7b298414d6b63b147
Xi Wang <g@g.net>
2016-10-27 17:13:05 +1300

    Create helper FixtureLoader

---
+++ src/AppBundle/Utils/FixtureLoader.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Utils;
+
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
+class FixtureLoader
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * TODO: environment control fixed on test dev
+     * Load default Alice/Faker fixture file
+     * @param string $filename
+     * @param string $environment
+     */
+    public function load($filename, $environment = 'dev')
+    {
+        $entityManager = $this->container->get('doctrine.orm.entity_manager');
+        $kernel = $this->container->get('kernel');
+        $fixtureFolder = __DIR__ . '/../DataFixtures/ORM/';
+
+        $this->container
+            ->get('hautelook_alice.doctrine.executor.fixtures_executor')
+            ->execute(
+                $entityManager,
+                $this->container->get('hautelook_alice.doctrine.orm.loader_generator')->generate(
+                    $this->container->get('hautelook_alice.fixtures.loader'),
+                    $this->container->get('hautelook_alice.alice.fixtures.loader'),
+                    $kernel->getBundles(),
+                    $environment),
+                $this->container->get('hautelook_alice.doctrine.orm.fixtures_finder')->resolveFixtures(
+                    $kernel,
+                    [ $fixtureFolder . $filename ]),
+                false, //If true, data will not be purged
+                function ($message) {
+                }, //Can be used for logging, if needed
+                true); //If true, truncates instead of deleting
+    }
+}
--- tests/unit/FixtureLoaderTest.php
+++ tests/unit/FixtureLoaderTest.php
@@ -21,6 +21,9 @@ class FixtureLoaderTest extends \Codeception\Test\Unit
         $this->tester->wantTo('load fixtures from default file and see them in DB');
         $fixtureLoader = $this->tester->grabService('app.fixture_loader');
         $fixtureLoader->load('fixtures_test_env.yml','dev');
-        $this->tester->seeInRepository('Application\Sonata\UserBundle\Entity\User', ['username' => 'root']);
+        $this->tester->seeInRepository('Application\Sonata\UserBundle\Entity\User', ['username' => 'admin']);
+        $this->tester->seeInRepository('Application\Sonata\UserBundle\Entity\User', ['username' => 'superadmin']);
+        $this->tester->seeInRepository('AppBundle\Entity\Category', ['name' => 'acme_category']);
+        $this->tester->seeInRepository('AppBundle\Entity\Product', ['name' => 'acme_product']);
     }
 }
