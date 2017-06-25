86a32ce8a7ff6c044a47023bf262da26519f21a8
Xi Wang <Xi Wang>
2017-06-25 14:23:12 +1200

    DEBUG #minor: fix testFixtureLoader

--- tests/unit/FixtureLoaderTest.php
+++ tests/unit/FixtureLoaderTest.php
@@ -10,19 +10,20 @@ class FixtureLoaderTest extends \Codeception\Test\Unit
 
     protected function _before()
     {
-        // Load fixtures
-        $fixtureLoader = $this->tester->grabService('app.fixture_loader');
-        $fixtureLoader->load('fixtures.yml','test');
     }
 
     protected function _after()
     {
-        $this->tester->purgeDB();
     }
 
     public function testFixtureLoader()
     {
         $this->tester->wantTo('load fixtures from default file and see them in DB');
+        // Reset DB and load fixtures
+        $this->tester->purgeDB();
+        $fixtureLoader = $this->tester->grabService('app.fixture_loader');
+        $fixtureLoader->load('fixtures.yml','test');
+        // Check existence of entities
         $this->tester->canSeeInRepository('Application\Sonata\UserBundle\Entity\User', ['username' => 'alice']);
         $this->tester->canSeeInRepository('Application\Sonata\UserBundle\Entity\User', ['username' => 'ryan']);
         $this->tester->canSeeNumRecords(4, 'app_category');
