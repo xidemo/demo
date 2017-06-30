26af8fe6f222796339f9572506e4f0ca2bb5d932
Xi Wang <Xi Wang>
2017-06-30 20:59:04 +1200

    TESTS: update Codeception, add db server version config to eliminate weird doctrine pdo exception, other minor adjustment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,6 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            $bundles[] = new \Hautelook\AliceBundle\HautelookAliceBundle();
         }
 
         return $bundles;
